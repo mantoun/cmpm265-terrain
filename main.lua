b7013415fc1f31bc4ac11bc4ada46eb7ec121a37
Matthew Antoun <matthew@antoun.org>
2016-01-25 17:02:56 -0800

    Refine controls

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ local debugText = true       -- Whether to draw the controls on the screen
 local debugInterval = 1/10   -- Time between updates
 local debugTimer = 0
 
+local timestep = .01    -- how much to age the map each keypress
+
 function love.load()
   love.keyboard.setKeyRepeat(true)
   -- Initialize controls
@@ -28,77 +30,99 @@ function love.load()
       terrain.createMap()
     end
   }, {
-    key="k",
-    description="scale -",
-    control=function()
-      terrain.scale = terrain.scale - .5
-      terrain.createMap()
-    end
-  }, {
-    key="l",
+    key="q",
     description="scale +",
     control=function()
-      terrain.scale = terrain.scale + .5
+      terrain.scale = terrain.scale + .25
       terrain.createMap()
     end
   }, {
-    key="o",
-    description="age -",
+    key="a",
+    description="scale -",
     control=function()
-      terrain.age = terrain.age - .1
+      terrain.scale = terrain.scale - .25
       terrain.createMap()
     end
   }, {
-    key="p",
-    description="age +",
-    control=function()
-      terrain.age = terrain.age + .1
-      terrain.createMap()
-    end
-  }, {
-    key="n",
-    description="octaves -",
-    control=function()
-      terrain.octaves = math.max(terrain.octaves - 1, 1)
-      terrain.createMap()
-    end
-  }, {
-    key="m",
+    key="w",
     description="octaves +",
     control=function()
       terrain.octaves = terrain.octaves + 1
       terrain.createMap()
     end
   }, {
-    key="a",
-    description="offset x -",
-    control=function()
-      terrain.offset[1] = terrain.offset[1] - .1
-      terrain.createMap()
-    end
-  }, {
     key="s",
-    description="offset x +",
+    description="octaves -",
     control=function()
-      terrain.offset[1] = terrain.offset[1] + .1
+      terrain.octaves = math.max(terrain.octaves - 1, 1)
       terrain.createMap()
     end
   }, {
-    key=",",
-    description="persistence -",
-    control=function()
-      terrain.persistence = math.max(terrain.persistence - .02, 0)
-      terrain.createMap()
-    end
-  }, {
-    key=".",
+    key="e",
     description="persistence +",
     control=function()
       terrain.persistence = terrain.persistence + .02
       terrain.createMap()
     end
   }, {
-    key="q",
+    key="d",
+    description="persistence -",
+    control=function()
+      terrain.persistence = math.max(terrain.persistence - .02, 0)
+      terrain.createMap()
+    end
+  }, {
+    key="r",
+    description="age +",
+    control=function()
+      terrain.age = terrain.age + timestep
+      terrain.createMap()
+    end
+  }, {
+    key="f",
+    description="age -",
+    control=function()
+      terrain.age = terrain.age - timestep
+      terrain.createMap()
+    end
+  }, {
+    key="t",
+    description="timestep +",
+    control=function() timestep = timestep + .01 end
+  }, {
+    key="g",
+    description="timestep -\n",
+    control=function() timestep = timestep - .01 end
+  }, {
+    key="up",
+    description="move map",
+    control=function()
+      terrain.offset[1] = terrain.offset[1] - .1
+      terrain.createMap()
+    end
+  }, {
+    key="down",
+    description="",
+    control=function()
+      terrain.offset[1] = terrain.offset[1] + .1
+      terrain.createMap()
+    end
+  }, {
+    key="left",
+    description="",
+    control=function()
+      terrain.offset[2] = terrain.offset[2] - .1
+      terrain.createMap()
+    end
+  }, {
+    key="right",
+    description="\n",
+    control=function()
+      terrain.offset[2] = terrain.offset[2] + .1
+      terrain.createMap()
+    end
+  }, {
+    key="escape",
     description="quit",
     control=function() love.event.push("quit") end
   }}
@@ -119,12 +143,14 @@ function love.update(dt)
     local controlsList = {}
     for i,v in ipairs(controls) do
       local d = v.description
-      if v.key == "k" then
+      if v.key == "q" then
         d = ("%s [%s]"):format(d, terrain.scale)
-      elseif v.key == "n" then
+      elseif v.key == "w" then
         d = ("%s [%s]"):format(d, terrain.octaves)
-      elseif v.key == "," then
+      elseif v.key == "e" then
         d = ("%s [%s]"):format(d, terrain.persistence)
+      elseif v.key == "t" then
+        d = ("%s [%s]"):format(d, timestep)
       end
       table.insert(controlsList, ("%s\t%s"):format(v.key, d))
     end
